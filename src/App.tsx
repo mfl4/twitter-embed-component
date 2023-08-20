47f1c334aaba26e40371bcfbe0c442f7b29e2633
mfl4 <mhdfarhanl6267@gmail.com>
2023-08-20 19:08:08 +0700

    project intialization

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <h1 className="title">Hello World</h1>
+      <div className="container">
+        <p className="paragraph">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde
+          consequuntur quae et incidunt necessitatibus ullam deleniti hic
+          aspernatur excepturi itaque?
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
